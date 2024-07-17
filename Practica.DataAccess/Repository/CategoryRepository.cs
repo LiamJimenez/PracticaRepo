3d6687b0317033b3bad1381083b0834369981e1f
LiamJimenez <liammarcos45@gmail.com>
2024-07-17 09:44:28 -0400

    Add project files.

---
+++ Practica.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Practica.DataAccess.Data;
+using Practica.DataAccess.Repository.IRepository;
+using PracticaModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db) 
+        { 
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj); 
+        }
+    }
+}
---
+++ PracticaWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Practica.DataAccess.Data;
+using Practica.DataAccess.Repository.IRepository;
+using Practica.Models;
+using Practica.Models.ViewModels;
+
+namespace PracticaWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+          
+            return View(objProductList);
+        }
+        
+        public IActionResult Create()
+        {
+
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            return View(productVM);
+        }
+        [HttpPost]
+        public IActionResult Create(ProductVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            { 
+                return NotFound(); 
+            }
+            _unitOfWork.Product.Remove(obj); 
+            _unitOfWork.Save(); 
+            TempData["success"] = "Product deleted successfully"; 
+            return RedirectToAction("Index"); 
+        }  
+    }
+}
