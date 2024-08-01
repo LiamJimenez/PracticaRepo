e2cc9469f9dda0bb40ac18d97c75cc8d45bf638b
LiamJimenez <liammarcos45@gmail.com>
2024-08-01 15:41:53 -0400

    Actualizar Login y Registro

--- PracticaModels/ApplicationUser.cs
+++ PracticaModels/ApplicationUser.cs
@@ -11,7 +11,7 @@ namespace Practica.Models
     public class ApplicationUser:IdentityUser 
     {
         [Required]
-        public int Name {  get; set; }
+        public string Name {  get; set; }
         public string? StreetAndress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
